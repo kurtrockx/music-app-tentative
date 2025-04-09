010f63057f945017123275319b6ec274aaccdb29
derpfear <kurtdebelen431@gmail.com>
2025-04-09 16:14:13 +0800

    added abort controller for when searching for a track

--- src/components/NavSearchbar.jsx
+++ src/components/NavSearchbar.jsx
@@ -1,27 +1,42 @@
 import { useNavigate } from "react-router-dom";
 import searchIcon from "../assets/searchIcon.svg";
+import { useState } from "react";
 
 export default function NavSearchbar() {
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
 
   function handleParamChange(e) {
-    const searchQuery = e.target.value;
-    if (searchQuery === "") {
+    if (e.target.value === "") {
       navigate("/newRelease");
       return;
     }
+    setQuery(e.target.value);
     navigate(`/search/${e.target.value}`);
+  }
+  function handleFormSubmission(e) {
+    e.preventDefault();
+    navigate(`/search/${query}`);
   }
 
   return (
-    <div className="flex items-center gap-2 rounded-sm border border-white px-2 py-2 duration-200 focus-within:bg-white/4 hover:bg-white/4">
+    <form
+      onSubmit={handleFormSubmission}
+      className="flex items-center gap-2 rounded-sm border border-white px-2 py-2 duration-200 focus-within:bg-white/4 hover:bg-white/4"
+    >
       <input
         type="text"
         className="w-32 px-px text-white outline-0 duration-200 focus:w-48"
         placeholder="Search"
         onChange={handleParamChange}
       />
-      <img src={searchIcon} alt="search-icon" className="h-6 cursor-pointer" />
-    </div>
+      <button>
+        <img
+          src={searchIcon}
+          alt="search-icon"
+          className="h-6 cursor-pointer"
+        />
+      </button>
+    </form>
   );
 }
--- src/pages/SongPage.jsx
+++ src/pages/SongPage.jsx
@@ -10,32 +10,39 @@ export default function SongPage({ accessToken }) {
   const { id } = useParams();
   const [songs, setSongs] = useState([]);
   const topSearch = songs[0];
-  const songList = songs.slice(1);
 
   useEffect(() => {
     if (!accessToken) return;
+
+    const controller = new AbortController();
+    const { signal } = controller;
 
     fetch(`https://api.spotify.com/v1/search?q=${id}&type=track`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
+      signal,
     })
       .then((response) => response.json())
       .then((data) => {
         setSongs(data.tracks.items);
       })
       .catch((error) => console.error("Error:", error));
+
+    return function () {
+      controller.abort();
+    };
   }, [accessToken, id, setSongs]);
 
   return !songs.length ? (
     <Loader />
   ) : (
-    <SectionContainer className={"flex h-10/12 flex-col gap-2 mt-auto"}>
+    <SectionContainer className={"mt-auto flex h-10/12 flex-col gap-2"}>
       <div className="flex">
         <TopSearchSong song={topSearch} />
       </div>
       <SongList>
-        {songList.map((song) => (
+        {songs.map((song) => (
           <Song song={song} key={song.id} />
         ))}
       </SongList>
